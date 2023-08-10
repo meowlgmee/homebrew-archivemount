0ef32ececaa8532427fb34e588da3e1c2a473c31
Dingtao <dingtao@DingtaoDeMbp16.local>
2023-08-10 16:10:34 +0800

    bugfix: fix bin install

--- Formula/archivemount.rb
+++ Formula/archivemount.rb
@@ -24,6 +24,8 @@ class Archivemount < Formula
   sha256 "c45332c0ecc522ab511effc97b32b089fb61eedd86695627be9715d7090598bf"
 
   def install
+    (prefix/"toolchain").install Dir["./*"]
+    # Dir.glob(prefix/"toolchain/bin/*") {|file| bin.install_symlink file}
     bin.install_symlink Dir["bin/*"]
   end
 end
