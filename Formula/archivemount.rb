a701b65b98036ec0ccf55199308716001728dd74
Dingtao <dingtao@DingtaoDeMbp16.local>
2023-08-10 15:54:08 +0800

    bugfix: Fix error

--- Formula/archivemount.rb
+++ Formula/archivemount.rb
@@ -24,7 +24,7 @@ class Archivemount < Formula
   sha256 "c45332c0ecc522ab511effc97b32b089fb61eedd86695627be9715d7090598bf"
 
   def install
-    (prefix/).install Dir["./*"]
+    (prefix).install Dir["./*"]
     Dir.glob(prefix/"bin/*") {|file| bin.install_symlink file}
   end
 end
