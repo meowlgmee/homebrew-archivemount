b4f87db062cd8ea624f1726c2d7a84b9346489f2
Dingtao <dingtao@DingtaoDeMbp16.local>
2023-08-10 15:51:06 +0800

    bugfix: Add binary bottle

--- Formula/archivemount.rb
+++ Formula/archivemount.rb
@@ -1,59 +1,30 @@
 require "digest"
 
-class OsxfuseRequirement < Requirement
+class CpuRequirement < Requirement
   fatal true
 
-  satisfy(build_env: false) { self.class.binary_osxfuse_installed? }
-
-  def self.binary_osxfuse_installed?
-    File.exist?("/usr/local/include/fuse/fuse.h") &&
-      !File.symlink?("/usr/local/include/fuse")
-  end
-
-  env do
-    unless HOMEBREW_PREFIX.to_s == "/usr/local"
-      ENV.append_path "HOMEBREW_LIBRARY_PATHS", "/usr/local/lib"
-      ENV.append_path "HOMEBREW_INCLUDE_PATHS", "/usr/local/include/fuse"
-    end
-  end
+  satisfy() { Hardware::CPU.arm? }
 
   def message
-    "macFUSE is required to build libguestfs. Please run `brew install --cask macfuse` first."
+    "Only apple m1/m2 supported!"
   end
 end
 
 class Archivemount < Formula
   desc "Archivemount is a piece of glue code between libarchive and FUSE . It can be used to mount a (possibly compressed) archive (as in .tar.gz or .tar.bz2) and use it like an ordinary filesystem."
   homepage "https://github.com/cybernoid/archivemount"
-  url "https://github.com/nexbeam/homebrew-archivemount/releases/download/v0.9.1/archivemount_v0.9.1.tar.gz"
-  sha256 "c46dead17fc949c78bf206ada336a041e75373e12386beca074348d794b22cd0"
-
-  depends_on "automake" => :build
-  depends_on "autoconf" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
-  depends_on "truncate" => :build
-  depends_on "libarchive"
+  version "0.9.1"
+  revision 1
 
   on_macos do
-    depends_on OsxfuseRequirement => :build
+      depends_on CpuRequirement
   end
+
+  url "https://github.com/nexbeam/homebrew-archivemount/releases/download/v0.9.1/archivemount-aarch64-darwin.tar.gz"
+  sha256 "c45332c0ecc522ab511effc97b32b089fb61eedd86695627be9715d7090598bf"
 
   def install
-    ENV["CFLAGS"] = "-I#{Formula["libarchive"].opt_include} -I/usr/local/include"
-    ENV["LDFLAGS"] = "-L#{Formula["libarchive"].opt_lib} -L/usr/local/lib"
-
-    ENV["FUSE_CFLAGS"] = "-I/usr/local/include"
-    ENV["FUSE_LIBS"] = "-L/usr/local/lib"
-
-    system "./configure", "--prefix=#{prefix}"
-    system "make"
-    system "make", "install"
-
-    bin.install_symlink Dir["bin/*"]
-  end
-
-  test do
-    system "#{bin}/archivemount", "-h"
+    (prefix/).install Dir["./*"]
+    Dir.glob(prefix/"bin/*") {|file| bin.install_symlink file}
   end
 end
